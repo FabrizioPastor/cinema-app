b73ea07ff319112dbb86dfa3a408984ab244a33d
FabrizioPastor <fabriziopastors@outlook.com>
2022-10-05 19:17:33 -0500

    Carga inicial del proyeco

---
+++ cinema-app/cinema-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinema-app
+//
+//  Created by FABRIZIO on 5/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
