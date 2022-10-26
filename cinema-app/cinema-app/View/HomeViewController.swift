a98698aadc4da4a32bcf35b88a44d6eaf80bbfc5
FabrizioPastor <fabriziopastors@outlook.com>
2022-10-25 23:25:53 -0500

    Agregando UITableView a HomeViewController

--- cinema-app/cinema-app/View/HomeViewController.swift
+++ cinema-app/cinema-app/View/HomeViewController.swift
@@ -9,12 +9,43 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let moviesTable: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemYellow
+        view.addSubview(moviesTable)
+        
+        moviesTable.delegate = self
+        moviesTable.dataSource = self
+        
+        
     }
 
+    //EVENTO QUE SE EJECUTA AL MOMENTO QUE SE AGREGAN SUBVIEWS
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //INSERTAR UN TABLEVIEW QUE OCUPA TODO EL ESPACIO DEL HOME VIEW
+        moviesTable.frame = view.bounds
+    }
 
 }
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "My text"
+        return cell
+    }
+    
+    
+}
