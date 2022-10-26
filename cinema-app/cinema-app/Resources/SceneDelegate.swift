d8fc99839fd1fd9530707f529f39d7b20f4289ae
FabrizioPastor <fabriziopastors@outlook.com>
2022-10-25 22:39:02 -0500

    Quitando storyboard del proyecto

--- cinema-app/cinema-app/Resources/SceneDelegate.swift
+++ cinema-app/cinema-app/Resources/SceneDelegate.swift
@@ -13,10 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windosScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windosScene.coordinateSpace.bounds)
+        window?.windowScene = windosScene //ESTO NO SE PARA QUÉ ES JAJAJA
+        window?.rootViewController = ViewController() //SETEO EL CONTROLADOR
+        window?.makeKeyAndVisible() //HAGO VISIBLE LA VENTANA
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
