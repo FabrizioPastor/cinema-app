861a0b6dbacc38781be73047235b6887383c0452
FabrizioPastor <fabriziopastors@outlook.com>
2022-10-30 10:37:12 -0500

    Agregando CollectionView a FooterView

--- cinema-app/cinema-app/View/Elements/FooterView.swift
+++ cinema-app/cinema-app/View/Elements/FooterView.swift
@@ -9,15 +9,35 @@ import UIKit
 
 
 class FooterView: UIView {
-
+    
+    private let footerCollectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 138, height: 180)
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "footerCell")
+        return collectionView
+    }()
+    
+    @IBOutlet weak var footerViewCollectionView: UIView!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureView()
+        //footerViewCollectionView.backgroundColor = .red
+        //footerCollectionView.backgroundColor = .blue
+        footerCollectionView.frame = footerViewCollectionView.bounds
+        footerViewCollectionView.addSubview(footerCollectionView)
+        
+        footerCollectionView.delegate = self
+        footerCollectionView.dataSource = self
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureView()
+        footerCollectionView.frame = footerViewCollectionView.bounds
+        footerViewCollectionView.addSubview(footerCollectionView)
     }
     
     private func configureView(){
@@ -25,5 +45,19 @@ class FooterView: UIView {
         view.frame = bounds
         addSubview(view)
     }
+}
+
+extension FooterView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "footerCell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+     
 }
 
