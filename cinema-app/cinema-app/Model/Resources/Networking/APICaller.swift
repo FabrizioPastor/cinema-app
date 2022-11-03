574f9ef41af0048c1afa92f166da038f82ebdbb2
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-03 18:08:16 -0500

    Creando método  prepareURLByCategory

--- cinema-app/cinema-app/Model/Resources/Networking/APICaller.swift
+++ cinema-app/cinema-app/Model/Resources/Networking/APICaller.swift
@@ -12,6 +12,48 @@ struct APICaller {
     static let shared = APICaller()
     
     private struct Constants {
-        let baseURL = "https://api.themoviedb.org"
+        static let baseURL = "https://api.themoviedb.org"
+    }
+    
+    enum APIError: Error {
+    case failedTogetData
+    }
+    
+    enum categoryEnum: Int {
+        case trendingMovies = 0
+        case upcomingMovies = 1
+        case topRated = 2
+    }
+    
+    //MARK: - Métodos de clase
+    func getDataFrom (category: categoryEnum, completion: @escaping (Result<[CategoryBody], Error>) -> Void) {
+        guard let url = URL(string: prepareURLByCategory(category)) else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                //let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                let results = try JSONDecoder().decode(CategoryResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(APIError.failedTogetData))
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
+    
+    private func prepareURLByCategory(_ category: categoryEnum) -> String {
+        switch category {
+        case .trendingMovies:
+            return "\(Constants.baseURL)/3/trending/movie/day?api_key=\(ENV.SERVICE_API_KEY)"
+        case .upcomingMovies:
+            return "\(Constants.baseURL)/3/movie/upcoming?api_key=\(ENV.SERVICE_API_KEY)"
+        case .topRated:
+            return "\(Constants.baseURL)/3/movie/top_rated?api_key=\(ENV.SERVICE_API_KEY)"
+        }
     }
 }
