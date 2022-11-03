c799abf03d526e70be86ca3f055fc06b261ee55f
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-03 18:13:21 -0500

    Agregando CategoryResponse Model

---
+++ cinema-app/cinema-app/Model/CategoryResponse.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryResponse.swift
+//  cinema-app
+//
+//  Created by FABRIZIO on 3/11/22.
+//
+
+import Foundation
+
+struct CategoryResponse: Codable {
+    let results: [CategoryBody]
+}
+
+struct CategoryBody: Codable {
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
